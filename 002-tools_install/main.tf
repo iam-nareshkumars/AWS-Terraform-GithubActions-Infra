734fccddd11f88cb88f4717176059e04d95d195b
naresh <Naresh@gmail.com>
2025-12-03 18:08:26 +0530

    terraform tool install and configure

---
+++ 002-tools_install/main.tf
@@ -0,0 +1,14 @@
+module "tools" {
+  for_each      = var.tools
+  source        = "./modules"
+  ami           = data.aws_ami.main.image_id
+  instance_type = each.value["instance_type"]
+  Name          = each.value["Name"]
+  zone_id       = data.aws_route53_zone.main.zone_id
+  domain        = var.domain
+  port_no       = each.value["port_no"]
+
+
+}
+
+
