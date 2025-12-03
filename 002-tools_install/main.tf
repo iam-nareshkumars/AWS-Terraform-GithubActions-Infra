1c5562a87bd13282bd5b6cd963fbac5e943ae7b0
naresh <Naresh@gmail.com>
2025-12-03 19:30:46 +0530

    terraform tool install and configure

--- 002-tools_install/main.tf
+++ 002-tools_install/main.tf
@@ -1,6 +1,6 @@
 module "tools" {
-  for_each      = var.tools
-  source        = "./modules"
+  for_each      =  var.tools
+  source        = "git::https://github.com/iam-nareshkumars/tools_setup.git"
   ami           = data.aws_ami.main.image_id
   instance_type = each.value["instance_type"]
   Name          = each.value["Name"]
