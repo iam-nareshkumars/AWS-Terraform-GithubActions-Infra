a31db03dd282b02483f215891675f64d7a383a4e
naresh <Naresh@gmail.com>
2025-12-03 15:32:12 +0530

    terraform security groups configure

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -2,18 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
  }
  
     }
 backend "s3" {
-   bucket = "roboshop-eternal-s3-terraformstate-dev"
-   key    = "vpc/terraform.tfstate"
+   bucket = "aws-infra-terraform-statefile"
+   key    = "vpc/dev/terraform.tfstate"
    encrypt = true
    region = "us-east-1"
    use_lockfile = true
-
-    
-    }
+}
 
 }
