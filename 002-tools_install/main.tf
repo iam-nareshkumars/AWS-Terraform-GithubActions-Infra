efa89923125ab7132f66f1bbe2ebe617e654a467
naresh <Naresh@gmail.com>
2025-12-03 19:49:09 +0530

    added

--- 002-tools_install/main.tf
+++ 002-tools_install/main.tf
@@ -1,6 +1,6 @@
 module "tools" {
   for_each      =  var.tools
-  source        = "git::https://github.com/iam-nareshkumars/tools_setup.git"
+  source        =  "./modules"
   ami           = data.aws_ami.main.image_id
   instance_type = each.value["instance_type"]
   Name          = each.value["Name"]
